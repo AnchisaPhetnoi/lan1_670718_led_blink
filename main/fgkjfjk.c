cac555ea373f0e8b6d2b2a1ad25cd94ac8cd50db
Anchisa <65030289@kmitl.ac.th>
2024-07-18 16:43:51 +0700

    LAB

---
+++ main/fgkjfjk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h" 
+
+
+void app_main(void)
+{
+    gpio_set_direction(5, GPIO_MODE_OUTPUT)
+      gpio_set_level(2, GPIO_MODE_OUTPUT);
+      int button2 = 0;
+    while (1) {
+
+    //  gpio_set_level(5,1);
+      // vTaskDelay(500 / pprtTICK_PERIOD_MS);
+       // gpio_set_level(5,0);
+       // vTaskDelay(500/ portTICK_PERIOD_MS);
+
+       button2 = gpio_set_level(2);
+       if(button2 == 1)
+       {
+        gpio_set_level(5,1);
+       }
+    }
+}
